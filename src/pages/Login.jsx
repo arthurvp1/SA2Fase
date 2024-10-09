b7e880888751a44acc716ac719037631490d252b
filipefln@gmail.com <luiz_oliveira34@estudante.sesisenai.org.br>
2024-10-09 15:55:13 -0300

    att

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,14 +4,77 @@ import './Login.css'
 function Login() {
     return (
         <div className='login-container'>
-            
+
 
             <div className='esquerda-container'>
-                
+                <div className='div-esquerda-top'>
+
+                    <img src="./imagens/iconSale.png" alt="" className='img-icon-sale' />
+
+                    <h1 className='h1-icon'>SaleSight</h1>
+
+                </div>
+                <div className='div-esquerda-meio'>
+                    <img src="./imagens/projeto.png" alt="" className='img-project' />
+
+                </div>
+                <div className='div-esquerda-bot'>
+                    <div className='div-p-bot-login1'>
+                        <p className='p-login-transformando'>Transformando Dados </p>
+                        <p className='p-login-transformando'>em Decisões</p>
+                    </div>
+                    <div className='div-p-bot-login2'>
+
+                        <p>Com nossa plataforma de relatório de venda, impulsione suas </p>
+                        <p>estratégias e maximize seus resultados</p>
+                    </div >
+
+                </div>
+
 
             </div>
             <div className='direita-contaier'>
-                <p>ghvjhgjhgy</p>
+                <div className='div-direita-esquerda'>
+
+                </div>
+                <div className="dados">
+                    <div className='div-h1-direita'>
+                        <h1>Entrar & Conectar-se aos melhores relatórios de venda</h1>
+                    </div>
+                    <div className='buttons-goo-in'>
+                        <button>G Entrar Com Google</button>
+                        <button> IN Entrar Com Linkedin</button>
+
+                    </div>
+                    <div className='div-inputs-login'>
+                        <div className='form'>
+                            <label htmlFor="user">Usuário:</label>
+                            <input type="text" id='user' />
+                            <label htmlFor="senha">Senha:</label>
+                            <input type="password" id='senha' />
+                        </div>
+                        <div className='form2'>
+                            <div className='lbl-check'>
+                                <input type="checkbox" id='check' />
+                                <label htmlFor="check">Lembrar-me</label>
+                            </div>
+                            <div className='div-but-entrar'>
+                                <button className='but-entrar'>Entrar</button>
+
+                            </div>
+                        </div>
+
+                    </div>
+                    <div className='div-before-bot'>
+
+                    </div>
+                    <div className='div-bot'>
+
+                    </div>
+                </div>
+                <div className='div-direita-direita'>
+
+                </div>
             </div>
         </div>
     )
