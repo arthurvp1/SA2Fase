2344fc290738ec146474804e32875db44f9ffa2a
arthurvp1 <arthurviecilipinheirodasilva@gmail.com>
2024-10-28 15:04:01 -0300

    copilot

--- src/pages/CadastroFunc.jsx
+++ src/pages/CadastroFunc.jsx
@@ -1,16 +1,60 @@
 import React from 'react'
-import Navbar from '../components/Navbar'
+import './Cadastrofunc.css'
+import { useState } from 'react'
+
 
 function CadastroFunc() {
+
+  const DataInput = () => {
+        const [data, setData] = useState("");
+    
+        const handleInputChange = (e) => {
+          let valor = e.target.value.replace(/\D/g, "");
+    
+          if (valor.length >= 3 && valor.length <= 4) {
+            valor = valor.slice(0, 2) + "/" + valor.slice(2);
+          } else if (valor.length >= 5) {
+            valor = valor.slice(0, 2) + "/" + valor.slice(2, 4) + "/" + valor.slice(4, 8);
+          }
+          setData(valor);
+        };
   return (
-    <div className='cadastrofunc-container'>
-      <Navbar/>
-      
-        <h2>ola </h2>
+    <div className="Cadastrofunc-container">
+
+      <aside className="esquerdo">
+
+        <div className="texts">
+          <label className="titulo">Cadastro de funcionários</label>
+          <p className="descricao">Digite os dados do funcionário abaixo e clique em
+            <label className="verde">&nbsp;Confirmar</label></p>
+        </div>
 
 
+        <section className="inputs">
+          <input className="username-input" placeholder="Digite seu nome" type="text" />
+          <input className="email-input" placeholder="Digite seu email" type="email" />
+          <input className="senha-input" placeholder="Digite sua senha" type="password" />
+          <input className="telefone" placeholder="Digite seu telefone" type="text" />
+          <DataInput />
+        </section>
+
+
+        <section className="buttons">
+          <button className="confirma">CONFIRMAR</button>
+          <button className="limpa" onClick={handleClear}>LIMPAR</button>
+        </section>
+      </aside>
+
+
+      {/* <aside className="direito">
+        <div className="profile-section">
+          <img className="profile-picture" src="profile-img.png" alt="Profile" />
+          <button className="change-photo-button">Trocar Foto</button>
+        </div>
+      </aside> */}
     </div>
   )
+}
 }
 
 export default CadastroFunc
