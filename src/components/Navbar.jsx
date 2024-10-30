60dd4b50a3ade1d312c709d9bf62dfd97d2387ef
arthurvp1 <arthurviecilipinheirodasilva@gmail.com>
2024-10-30 16:41:26 -0300

    atualizando navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,39 +3,74 @@ import './Navbar.css'
 function Navbar() {
   return (
     <div className="navbar-container">
+
       <div className="logo-navbar">
         <div className="icon-navbar">
           <img src="./public/imagens/icon2.png" alt="" />
         </div>
+
       </div>
+
       <div className="div-label-navbar">
         <h2>Sale<span className="span-navbar">Sigth</span></h2>
       </div>
 
       <nav className="navbar">
-
         <div className="inicio">
-          <Link to="/"><img src="./imagens/Group 2.png" /> Inicio</Link>
+          <Link to="/">
+
+            <div className="iconesidebar">
+              <img src="./imagens/Group 2.png" alt="Início" />
+            </div>
+            
+            <div className="textsidebar">Início</div>
+
+          </Link>
         </div>
 
         <div className="desempenho">
-          <Link to="/desempenho"><img src="./imagens/TrendUp.png" />Desempenho</Link>
+          <Link to="/desempenho">
+            <div className="iconesidebar">
+              <img src="./imagens/TrendUp.png" alt="Desempenho" />
+            </div>
+            <div className="textsidebar">Desempenho</div>
+          </Link>
         </div>
 
         <div className="produtos">
-          <Link to="/produtos"><img src="./imagens/CirclesFour.png" />Produtos</Link>
+          <Link to="/produtos">
+            <div className="iconesidebar">
+              <img src="./imagens/CirclesFour.png" alt="Produtos" />
+            </div>
+            <div className="textsidebar">Produtos</div>
+          </Link>
         </div>
 
         <div className="vendidos">
-          <Link to="/maisVendidos"> <img src="./imagens/directions_car.png" alt="" />Mais Vendidos</Link>
+          <Link to="/maisVendidos">
+            <div className="iconesidebar">
+              <img src="./imagens/directions_car.png" alt="Mais Vendidos" />
+            </div>
+            <div className="textsidebar">Mais Vendidos</div>
+          </Link>
         </div>
 
         <div className="cadastro">
-          <Link to="/cadastroFunc"><img src="./imagens/Settings.png" alt="" />Cad Func</Link>
+          <Link to="/cadastroFunc">
+            <div className="iconesidebar">
+              <img src="./imagens/Settings.png" alt="Cad Func" />
+            </div>
+            <div className="textsidebar">Cad Func</div>
+          </Link>
         </div>
 
         <div className="logout">
-          <Link to="/login"><img src="./imagens/Log out.png" alt="" />Log Out</Link>
+          <Link to="/login">
+            <div className="iconesidebar">
+              <img src="./imagens/Log out.png" alt="Log Out" />
+            </div>
+            <div className="textsidebar">Log Out</div>
+          </Link>
         </div>
 
       </nav>
