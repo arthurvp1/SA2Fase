a57ce6bb429a595db12615f29dcf63f52eb0d5df
JOABE DOS SANTOS COSTA <joabe_s_costa@estudante.sesisenai.org.br>
2024-11-05 16:34:43 -0300

    add

--- src/pages/Desempenho.jsx
+++ src/pages/Desempenho.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import './Desempenho.css'
 import Navbar from '../components/Navbar'
-import { useState, useContext } from 'react'
-import { GlobalContext } from '../contexts/GlobalContext'
 import Funcionario from '../components/Funcionario'
 
 function Desempenho() {
-  const {isVisible, setIsVisible} = useContext(GlobalContext);
+  
+  
   
   return (
 
@@ -22,10 +21,6 @@ function Desempenho() {
 
           <div className="div-funcionarios">
           <Funcionario img={'imgn n carrega'} nome ={'Luiz Filipe oliveira'} email = {'filipefln@gmail.com'} fone = {'048991836492'} cpf = {'98989889989'}/>
-          <Funcionario />
-          <Funcionario />
-          <Funcionario />
-          <Funcionario />
           <Funcionario />
           <Funcionario />
           <Funcionario />
