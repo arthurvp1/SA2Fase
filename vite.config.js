56794ca801569ddb378c7be7f4593bb32137345c
JOABE DOS SANTOS COSTA <joabe_s_costa@estudante.sesisenai.org.br>
2024-11-14 16:43:16 -0300

    comandos dos graficos

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+//npm install --save react-apexcharts apexcharts
+//npm install apexcharts --save
+
 export default defineConfig({
   plugins: [react()],
 })
